5e5c44f40e790ebadf3af3d180b5d18e97cfa056
Guilherme758 <68336486+Guilherme758@users.noreply.github.com>
2023-08-16 20:18:30 -0300

    Add files via upload

---
+++ atividade_chars_especiais.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	printf(" _______________________________\n");
+	printf("|   PROGRAMAÇÃO COM ESPECIAIS   |\n");
+	printf("|                               |\n");
+	printf("|\\n           -    <ENTER>      |\n");
+	printf("|\\\\           -    \\            |\n");
+	printf("|\\t           -    <TAB>        |\n");
+	printf("|                               |\n");
+	printf("|ENCERRADO!!                    |\n");
+	printf("|_______________________________|\n");
+}
