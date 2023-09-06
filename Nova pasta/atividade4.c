3dd9fd88a602c6a55d4fb100f04bea3e801edc60
Guilherme758 <68336486+Guilherme758@users.noreply.github.com>
2023-09-06 20:21:19 -0300

    Update atividade4.c

--- Nova pasta/atividade4.c
+++ Nova pasta/atividade4.c
@@ -2,20 +2,68 @@
 #include <locale.h>
 
 int main(){
-	/* Escreva um programa que o usu·rio escolhe uma opÁ„o entre 2 (calcular sal·rio lÌquido ou calcular sal·rio bruto), 
-	ent„o recebe o valor que o funcion·rio recebe por hora trabalhada e apÛs recebe a quantidade de horas trabalhadas no mÍs*/
-	float valor_hora;
+	/* Escreva um programa que o usu√°rio escolhe uma op√ß√£o entre 2 (calcular sal√°rio l√≠quido ou calcular sal√°rio bruto), 
+	ent√£o recebe o valor que o funcion√°rio recebe por hora trabalhada e ap√≥s recebe a quantidade de horas trabalhadas no m√™s*/
+	float valor_hora, salario_bruto, salario_liquido, aliquota_ir, aliquota_inss, desconto_ir, desconto_inss;
 	int horas_trabalhadas, opcao;
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	printf("Escolha 1 para sal·rio bruto ou 2 para sal·rio lÌquido:\n");
-	scanf("%d", &opcao);
-	
 	printf("Insira o valor hora:\n");
-	scanf("%f", &opcao);
+	scanf("%f", &valor_hora);
 	
-	printf("Insira as horas trabalhadas no mÍs:\n");
+	printf("Insira as horas trabalhadas no m√™s:\n");
+	scanf("%d", &horas_trabalhadas);
+	
+	salario_bruto = horas_trabalhadas * valor_hora;
+	
+	printf("Escolha 1 para sal√°rio bruto ou 2 para sal√°rio l√≠quido:\n");
 	scanf("%d", &opcao);
 	
+	if(opcao == 1){
+	    printf("O sal√°rio bruto √© %.2f", salario_bruto);
+	}
+	else if(opcao == 2){
+	    if(salario_bruto <= 2112.00){
+		    aliquota_ir = 0.0;
+	    }
+	    else if(salario_bruto >= 2112.01 && salario_bruto <= 2826.65){
+		    aliquota_ir = 0.075;
+	    }
+    	else if(salario_bruto >= 2826.66 && salario_bruto <= 3751.05){
+    		aliquota_ir = 0.15;
+    	}
+    	else if(salario_bruto >= 3751.06 && salario_bruto <= 4664.68){
+    		aliquota_ir = 0.225;
+    	}
+    	else{
+    		aliquota_ir = 0.275;
+    	}
+    	
+    	if(salario_bruto <= 1320){
+    	    aliquota_inss = 0.075;
+    	}
+    	else if(salario_bruto >= 1320.01 && salario_bruto <= 2571.29){
+    	    aliquota_inss = 0.09;
+    	}
+    	else if(salario_bruto >= 2571.30 && salario_bruto <= 3856.94){
+    	    aliquota_inss = 0.12;
+    	}
+    	else if(salario_bruto >= 3856.95 && salario_bruto <= 7507.49){
+    	    aliquota_inss = 0.14;
+    	}
+    	else{
+    	    aliquota_inss = 0;
+    	}
+    	
+    	desconto_ir   = salario_bruto * aliquota_ir;
+    	desconto_inss = salario_bruto * aliquota_inss;
+    	
+    	salario_liquido = salario_bruto - desconto_ir - desconto_inss;
+    	
+    	printf("Seu sal√°rio l√≠quido √© de %.2f", salario_liquido);
+	}
+	else{
+	    printf("Op√ß√£o inv√°lida");
+	}
 }
