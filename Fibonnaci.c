555f146b55e58a53d8734ee8c49b848d028321a2
Guilherme758 <68336486+Guilherme758@users.noreply.github.com>
2023-09-26 12:49:31 -0300

    Update Fibonnaci.c

--- Fibonnaci.c
+++ Fibonnaci.c
@@ -3,23 +3,23 @@
 
 int main(){
 	//Calcular Fibonnaci
-	long numero, index;
+	int numero, index;
 	unsigned long long aux1, aux2 = 0, resultado = 0;
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	printf("Digite um número: \n");
+	printf("Digite um nÃºmero: \n");
 	scanf("%d", &numero);
 	
 	for(index=1;index<=numero;index++){
 		if(index == 1){
 			aux1 = 1;
 			aux2 = 0;
-			printf("Posição %d: %llu\n", index, aux1);
+			printf("PosiÃ§Ã£o %d: %llu\n", index, aux1);
 		}
 		else{
 			resultado = aux1 + aux2;
-			printf("Posição %d: %llu\n", index, resultado);
+			printf("PosiÃ§Ã£o %d: %llu\n", index, resultado);
 			aux2 = aux1;
 			aux1 = resultado;
 		}
