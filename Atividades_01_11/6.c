6254d59ee763840f76516c24972f1e78c604d6da
Guilherme758 <68336486+Guilherme758@users.noreply.github.com>
2023-11-08 21:04:07 -0300

    Update 6.c

--- Atividades_01_11/6.c
+++ Atividades_01_11/6.c
@@ -4,39 +4,26 @@
 #include <time.h>
 
 int sorteiaFace(){
-    int faces[5] = {0, 0, 0, 0, 0};
-    int i;
+    int faces[6] = {0, 0, 0, 0, 0, 0};
+	int i, j;
     
     for(i = 0; i < 1000000; i++){
         faces[rand() % 6] += 1;
     }
-    
+        
     printf("Quantidade de sorteios 1000000: \n");
-    printf("Face 1: %d \n", faces[0]);
-    printf("Face 2: %d \n", faces[1]);
-    printf("Face 3: %d \n", faces[2]);
-    printf("Face 4: %d \n", faces[3]);
-    printf("Face 5: %d \n", faces[4]);
-    printf("Face 6: %d \n", faces[5]);
     
-    return 1;
+	for(j = 0; j < 6; j++){
+		printf("Face %d: %d \n", j, faces[j]);
+	}
+    
+	return 1;
 }
 
-int main(){
-    int faces[5];
-    
+int main(){    
     setlocale(LC_ALL, "Portuguese");
     
     srand(time(NULL));
     
     sorteiaFace();
-    
-    // printf("Quantidade de sorteios: \n");
-    // printf("Face 1: %d \n", faces[0]);
-    // printf("Face 2: %d \n", faces[1]);
-    // printf("Face 3: %d \n", faces[2]);
-    // printf("Face 4: %d \n", faces[3]);
-    // printf("Face 5: %d \n", faces[4]);
-    // printf("Face 6: %d \n", faces[5]);
-    
 }
